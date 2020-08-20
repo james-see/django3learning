f4cbc1210ac9270b8d81cd3035f19965923f1f27
James Campbell <james@jamescampbell.us>
2020-08-20 11:27:30 -0400

    updates for user views

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
+from django.contrib.auth import authenticate
 # Create your views here.
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data("username")
+        password = form.cleaned_data("password")
+
+        user = authenticate(username=username, password=password)
